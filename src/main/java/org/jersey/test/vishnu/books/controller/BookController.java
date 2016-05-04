0710355855520a150e1c165d4df30f08ad20ab41
unknown <kvishnukant@yahoo.com>
2016-05-04 07:52:50 +0530

    first commit

---
+++ src/main/java/org/jersey/test/vishnu/books/controller/BookController.java
@@ -0,0 +1,40 @@
+package org.jersey.test.vishnu.books.controller;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.jersey.test.vishnu.books.pojo.BookPojo;
+
+@Path("/booksdetail")
+public class BookController {
+
+	@GET
+	//@Produces(MediaType.TEXT_XML)
+	@Produces(MediaType.TEXT_XML)
+	@Path("/instring")
+	public String getName()
+	{
+		BookPojo bookpojo=new BookPojo(10, "java", "author");
+		
+		//return Response.status(200).type(MediaType.TEXT_XML).entity(bookpojo).build();
+		return bookpojo.toString();
+	}
+	
+	
+	@GET
+	//@Produces(MediaType.TEXT_XML)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/inobject")
+	public Response getNameObject()
+	{
+		BookPojo bookpojo=new BookPojo(10, "java", "author");
+		Response.ResponseBuilder res= Response.status(Status.OK);
+		res.entity(bookpojo);
+		//return Response.status(200).type(MediaType.TEXT_XML).entity(bookpojo).build();
+		return res.build();
+	}
+}
---
+++ src/main/java/org/jersey/test/vishnu/books/pojo/BookPojo.java
@@ -0,0 +1,40 @@
+package org.jersey.test.vishnu.books.pojo;
+
+public class BookPojo {
+	
+	private int id;
+	
+	private String name;
+	private String author;
+	public BookPojo(int id, String name, String author) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.author = author;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	@Override
+	public String toString() {
+		return "BookPojo [id=" + id + ", name=" + name + ", author=" + author + "]";
+	}
+	
+	
+
+}
